935df0a404b64448f1ee74b00e2c882e0e793bb6
Richard Goldberg <Richard@Richards-MacBook-Air-2.local>
2013-10-10 10:18:41 -0400

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Richard Goldberg on 10/10/13.
+//  Copyright (c) 2013 Richard Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Richard Goldberg on 10/10/13.
+//  Copyright (c) 2013 Richard Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
